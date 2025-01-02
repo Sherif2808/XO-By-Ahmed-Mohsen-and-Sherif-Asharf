bcd337efc59bde70b97ae46320eaa27dd6c7ab32
Sherif <sherifashraf1091@gmail.com>
2025-01-02 19:16:55 +0200

    First prototype

--- XO By Ahmed Mohsen and Sherif Asharf/Form1.Designer.cs
+++ XO By Ahmed Mohsen and Sherif Asharf/Form1.Designer.cs
@@ -29,12 +29,225 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnclr = new System.Windows.Forms.Button();
+            this.btnres = new System.Windows.Forms.Button();
+            this.CPUTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(105, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player Wins";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(319, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPU Wins";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn1.Location = new System.Drawing.Point(108, 75);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(85, 73);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "?";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn2.Location = new System.Drawing.Point(199, 75);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(85, 73);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "?";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn3.Location = new System.Drawing.Point(290, 75);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(85, 73);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "?";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn4.Location = new System.Drawing.Point(108, 154);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(85, 73);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "?";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn5.Location = new System.Drawing.Point(199, 154);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(85, 73);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "?";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn6.Location = new System.Drawing.Point(290, 154);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(85, 73);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "?";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn7.Location = new System.Drawing.Point(108, 233);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(85, 67);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "?";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn8.Location = new System.Drawing.Point(199, 233);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(85, 67);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "?";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn9.Location = new System.Drawing.Point(290, 233);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(85, 67);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "?";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.PlayerClickButton);
+            // 
+            // btnclr
+            // 
+            this.btnclr.Location = new System.Drawing.Point(108, 323);
+            this.btnclr.Name = "btnclr";
+            this.btnclr.Size = new System.Drawing.Size(85, 23);
+            this.btnclr.TabIndex = 12;
+            this.btnclr.Text = "Clear";
+            this.btnclr.UseVisualStyleBackColor = true;
+            this.btnclr.Click += new System.EventHandler(this.btnclr_Click);
+            // 
+            // btnres
+            // 
+            this.btnres.Location = new System.Drawing.Point(290, 323);
+            this.btnres.Name = "btnres";
+            this.btnres.Size = new System.Drawing.Size(85, 23);
+            this.btnres.TabIndex = 13;
+            this.btnres.Text = "Restart Game";
+            this.btnres.UseVisualStyleBackColor = true;
+            this.btnres.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CPUTimer
+            // 
+            this.CPUTimer.Interval = 1000;
+            this.CPUTimer.Tick += new System.EventHandler(this.CPUmove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(205, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Turn";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(523, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnres);
+            this.Controls.Add(this.btnclr);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnclr;
+        private System.Windows.Forms.Button btnres;
+        private System.Windows.Forms.Timer CPUTimer;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- XO By Ahmed Mohsen and Sherif Asharf/Form1.cs
+++ XO By Ahmed Mohsen and Sherif Asharf/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,257 @@ namespace XO_By_Ahmed_Mohsen_and_Sherif_Asharf
 {
     public partial class Form1 : Form
     {
+        public enum player
+        {
+            X, O
+        }
+        player CurrentPlayer;
+        Random random = new Random();
+        int PlayerWinCount = 0;
+        int CPUWinCount = 0;
+        List<Button> buttons;
         public Form1()
         {
             InitializeComponent();
+            RestartGame();
+            RandomStart();
+        }
+        private void RandomStart()
+        {
+            int start = random.Next(2);
+            if (start == 0)
+            {
+                CurrentPlayer = player.X;
+                label3.Text = "Player's Turn";
+            }
+            else
+            {
+                CurrentPlayer = player.O;
+                label3.Text = "CPU's Turn";
+                CPUTimer.Start();
+            }
+        }
+        private void PlayerClickButton(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (CurrentPlayer == player.X)
+            {
+                button.Text = CurrentPlayer.ToString();
+                button.Enabled = false;
+                button.BackColor = Color.Cyan;
+                buttons.Remove(button);
+                CheckGame();
+                CurrentPlayer = player.O;
+                label3.Text = "CPU's Turn";
+                CPUTimer.Start();
+            }
+        }
+        private void CPUmove(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0 && CurrentPlayer == player.O)
+            {
+                int index = random.Next(buttons.Count);
+                buttons[index].Enabled = false;
+                buttons[index].Text = CurrentPlayer.ToString();
+                buttons[index].BackColor = Color.DarkSalmon;
+                buttons.RemoveAt(index);
+                CheckGame();
+                CurrentPlayer = player.X;
+                label3.Text = "Player's Turn";
+                CPUTimer.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclr_Click(object sender, EventArgs e)
+        {
+            PlayerWinCount = 0;
+            CPUWinCount = 0;
+            label1.Text = "Player Wins: 0";
+            label2.Text = "CPU Wins: 0";
+            RandomStart();
+            RestartGame();
+        }
+        private void CheckGame()
+        {
+            if (btn1.Text == "X" && btn2.Text == "X" && btn3.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn4.Text == "X" && btn5.Text == "X" && btn6.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn7.Text == "X" && btn8.Text == "X" && btn9.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Player Wins");
+                PlayerWinCount++;
+                label1.Text = "Player Wins: " + PlayerWinCount;
+                RestartGame();
+
+            }
+            else if (btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+
+            }
+            else if (btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O")
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("CPU Wins");
+                CPUWinCount++;
+                label2.Text = "CPU Wins: " + CPUWinCount;
+                RestartGame();
+            }
+            else if (!buttons.Any(b => b.Enabled))
+            {
+                CPUTimer.Stop();
+                MessageBox.Show("Draw");
+                RestartGame();
+            }
+        }
+        private void RestartGame()
+        {
+            buttons = new List<Button> { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            foreach (Button x in buttons)
+            {
+                x.Enabled = true;
+                x.Text = "?";
+                x.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
